4a322165665ba891787cd508809739cf52f447a3
Eloi <eloipagniez@gmail.com>
2014-05-30 12:43:29 +0200

    Upload fichier OK

--- import.php
+++ import.php
@@ -3,12 +3,33 @@
     include('header.php');
     include('connection_BDD.php');
 ?>
+<script>
+    $(document).ready(
+    function(){
+        $('input:file').change(
+            function(){
+                if ($(this).val()) {
+                    $('input:submit').attr('disabled',false);
+                    // or, as has been pointed out elsewhere:
+                    // $('input:submit').removeAttr('disabled'); 
+                } 
+            }
+            );
+    });
+</script>
 <html>
     <div class="nouveau">
         <div class="milieuPage">
             <h2 class="text-center">Importer des fichiers</h2>
             <br/>
+            <h5 align="center">Veuillez choisir un fichier au format .csv</h5>
+            <h5 align="center">Le contenu de ce fichier sera ajouté à l'application et remplacera le contenu existant (opération irreversible !)</h5><br/>
             <form action="uploadFile.php" method="post" class="milieuPage" enctype="multipart/form-data">
+                <select name="choix" class="form-control">
+                    <option value="matieres">Liste de matières</option>
+                    <option value="utilisateurs">Liste d'utilisateurs</option>
+                    <option value="promos">Liste de promotions</option>
+                </select><br/>
                 <div style="position:relative; margin-left:42.5%;">
                     <a class='btn btn-default' href='javascript:;'>
                         Parcourir...
@@ -18,7 +39,7 @@
                     <span class='label label-info' id="upload-file-info"></span>
                 </div>
                 <br/>
-                <input type="submit" class="btn btn-primary center-block" value="Envoyer" />
+                <input type="submit" disabled class="btn btn-primary center-block" value="Envoyer" />
             </form>     
         </div>
     </div>
--- uploadFile.php
+++ uploadFile.php
@@ -1,12 +1,45 @@
 <?php
+    include 'Connection_BDD.php';
     $allowedExts = array("csv", "xls", "xlsx", "pdf");
     $temp = explode(".", $_FILES["file"]["name"]);
     $extension = end($temp);
 
-    if (file_exists("upload/" . $_FILES["file"]["name"])) {
-      echo $_FILES["file"]["name"] . " already exists. ";
+    move_uploaded_file($_FILES["file"]["tmp_name"],
+    "upload/" . $_FILES["file"]["name"]);
+    
+    $f = fopen("upload/" . $_FILES["file"]["name"], 'r');
+    if ($f) {
+        $i=0;
+        $data = array();
+        
+        while ($line = fgetcsv($f)) {
+            $data[$i] = $line[0];
+            $i++;
+        }
+        fclose($f);
     } else {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
+        echo "Erreur dans l'ouverture du fichier";
     }
-?> 
+        
+    if($_POST["choix"]=='matieres'){
+        $table = "matiere (nom)";
+    } else if ($_POST["choix"]=='utilisateurs'){
+        $table = "utilisateur (nom)";
+    } else if ($_POST["choix"]=='promos'){
+        $table = "promo (nom)";
+    }
+    
+    $SQL = "INSERT into ".$table." VALUES ";
+    
+    for($i=0; $i<sizeof($data)-1; $i++){
+        $SQL.="('".$data[$i]."'),";
+    }
+    
+    $SQL.="('".$data[$i]."')";
+    
+    var_dump($SQL);
+    $rs = $cnx->query($SQL);
+    echo 'Operation effectuee';
+?>
+
+<a href="accueil.php">Retour a l'accueil</a>
